907b0fd88de0427621e5b5816cf1b329f5b09c65
ValentinRio <valentinrio.perso@gmail.com>
2024-05-17 17:08:08 +0200

    Create simple example that displays a window

---
+++ examples/simple.rs
@@ -0,0 +1,121 @@
+use wgpu::{
+    CommandEncoderDescriptor, CompositeAlphaMode, DeviceDescriptor, Features, Instance,
+    InstanceDescriptor, Limits, LoadOp, Operations, PresentMode,
+    RenderPassColorAttachment, RenderPassDescriptor, RequestAdapterOptions, SurfaceConfiguration,
+    TextureFormat, TextureUsages, TextureViewDescriptor,
+};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    window::WindowBuilder,
+};
+
+use std::sync::Arc;
+
+fn main() {
+    pollster::block_on(run());
+}
+
+async fn run() {
+    // Set up window
+    let (width, height) = (800, 600);
+    let event_loop = EventLoop::new().unwrap();
+    let window = Arc::new(
+        WindowBuilder::new()
+            .with_inner_size(LogicalSize::new(width as f64, height as f64))
+            .with_title("WGPU Font Renderer")
+            .build(&event_loop)
+            .unwrap(),
+    );
+    let size = window.inner_size();
+    let scale_factor = window.scale_factor();
+
+    // Set up surface
+    let instance = Instance::new(InstanceDescriptor::default());
+    let adapter = instance
+        .request_adapter(&RequestAdapterOptions::default())
+        .await
+        .unwrap();
+    let (device, queue) = adapter
+        .request_device(
+            &DeviceDescriptor {
+                label: None,
+                required_features: Features::empty(),
+                required_limits: Limits::downlevel_defaults(),
+            },
+            None,
+        )
+        .await
+        .unwrap();
+
+    let surface = instance
+        .create_surface(window.clone())
+        .expect("Create surface");
+    let swapchain_format = TextureFormat::Bgra8UnormSrgb;
+    let mut config = SurfaceConfiguration {
+        usage: TextureUsages::RENDER_ATTACHMENT,
+        format: swapchain_format,
+        width: size.width,
+        height: size.height,
+        present_mode: PresentMode::Fifo,
+        alpha_mode: CompositeAlphaMode::Opaque,
+        view_formats: vec![],
+        desired_maximum_frame_latency: 2,
+    };
+    surface.configure(&device, &config);
+
+    let _physical_width = (width as f64 * scale_factor) as f32;
+    let _physical_height = (height as f64 * scale_factor) as f32;
+
+    event_loop
+        .run(move |event, target| {
+            if let Event::WindowEvent {
+                window_id: _,
+                event,
+            } = event
+            {
+                match event {
+                    WindowEvent::Resized(size) => {
+                        config.width = size.width;
+                        config.height = size.height;
+                        surface.configure(&device, &config);
+                        window.request_redraw();
+                    }
+                    WindowEvent::RedrawRequested => {
+                        
+                        // Prepare should happen here
+                        
+                        let frame = surface.get_current_texture().unwrap();
+                        let view = frame.texture.create_view(&TextureViewDescriptor::default());
+                        let mut encoder = device
+                            .create_command_encoder(&CommandEncoderDescriptor { label: None });
+                        {
+                            let mut _pass = encoder.begin_render_pass(&RenderPassDescriptor {
+                                label: None,
+                                color_attachments: &[Some(RenderPassColorAttachment {
+                                    view: &view,
+                                    resolve_target: None,
+                                    ops: Operations {
+                                        load: LoadOp::Clear(wgpu::Color::BLACK),
+                                        store: wgpu::StoreOp::Store,
+                                    },
+                                })],
+                                depth_stencil_attachment: None,
+                                timestamp_writes: None,
+                                occlusion_query_set: None,
+                            });
+
+                            // Render should happen here
+                        }
+
+                        queue.submit(Some(encoder.finish()));
+                        frame.present();
+                    }
+                    WindowEvent::CloseRequested => target.exit(),
+                    _ => {}
+                }
+            }
+        })
+        .unwrap();
+}
